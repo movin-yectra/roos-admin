2e52ae11452780c90cb21b45a14867d48ab12763
movin-yectra <movin@yectra.com>
2024-02-26 13:28:03 +0530

    add app only

--- src/index.tsx
+++ src/index.tsx
@@ -1,65 +1,14 @@
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import "./styles/main.scss";
-
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "react-loading-skeleton/dist/skeleton.css";
-import "react-datepicker/dist/react-datepicker.css";
-
-//Phone number css
-import "react-phone-input-2/lib/style.css";
-
-//react text editer css
-import "react-quill/dist/quill.snow.css";
-
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./store/index";
-
-import LoadingAppAlertProvider from "./common/hooks/useAppAlert";
-
-//azure msal and config
-import { PublicClientApplication, EventType } from "@azure/msal-browser";
-import { msalConfig } from "./config/auth";
-import { MsalProvider } from "@azure/msal-react";
-
-//msal logic
-const msalInstance = new PublicClientApplication(msalConfig);
-
-if (
-  !msalInstance.getActiveAccount() &&
-  msalInstance.getAllAccounts().length > 0
-) {
-  msalInstance.setActiveAccount(msalInstance.getActiveAccount());
-}
-
-msalInstance.addEventCallback((event) => {
-  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
-    const accounts = event.payload;
-    msalInstance.setActiveAccount(accounts[0]);
-  }
-});
-
-function RootComponent() {
-  return (
-    <Router>
-      <Provider store={store}>
-        <LoadingAppAlertProvider>
-          <MsalProvider instance={msalInstance}>
-            <App />
-          </MsalProvider>
-        </LoadingAppAlertProvider>
-      </Provider>
-    </Router>
-  );
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
-
-root.render(<RootComponent />);
-
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
